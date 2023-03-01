08c3f310fde9b26871c7a1953d0a76298f3c1ef6
Gowroji Sunil <48122892+sgowroji@users.noreply.github.com>
2023-03-01 10:50:02 +0530

    Add files via upload

---
+++ intellij_automation.sh
@@ -0,0 +1,34 @@
+﻿#!/bin/sh
+ 
+# check for where the latest version of IDEA is installed
+IDEA=`ls -1d /Applications/IntelliJ\ * | tail -n1`
+wd=/Users/sgowroji/IdeaProjects/intellij/examples/java/greetings_project
+
+# were we given a directory?
+if [ -d "$1" ]; then
+#  echo "checking for things in the working dir given"
+  wd=`ls -1d "$1" | head -n1`
+fi
+
+# were we given a file?
+if [ -f "$1" ]; then
+#  echo "opening '$1'"
+  open -a "$IDEA" "$1"
+else
+    # let's check for stuff in our working directory.
+    pushd $wd > /dev/null
+
+    # does our working dir have an .idea directory?
+    if [ -d ".idea" ]; then
+#      echo "opening via the .idea dir"
+      open -a "$IDEA" .
+
+    # can't do anything smart; just open IDEA
+    else
+#      echo 'cbf'
+      open "$IDEA"
+    fi
+
+    popd > /dev/null
+fi 
+done
