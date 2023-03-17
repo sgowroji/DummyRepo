af3323dfa206f6460fe94d342cc09b17a85c20ef
Gowroji Sunil <48122892+sgowroji@users.noreply.github.com>
2023-03-17 15:07:10 +0530

    Create

---
+++ .github/workflows/scripts/check_priority.js
@@ -0,0 +1,59 @@
+
+module.exports = async ({ github, context }) => {
+    // console.log('entered unmark for issueid = ', context.payload.issue.number);
+    // console.log('issue author is ', context.payload.issue.user.login);
+    // console.log('issue latest commenter is ', context.payload.sender.login);
+
+
+    let issues = await github.rest.issues.listForRepo({
+        owner:context.repo.owner,
+        repo:context.repo.repo,
+        state:"open",
+        labels:"p0"
+    });
+
+    
+    if (issues.status != 200)
+        return
+
+   let issueList = issues.data
+    console.log("issue list")
+    for (let i = 0; i < issueList.length; i++) {
+
+        let number = issueList[i].number;
+        let resp = await github.rest.issues.listEventsForTimeline({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: number,
+        });
+        let events = resp.data;
+        console.log("event")
+        for (let i = 0; i < events.length; i++) {
+            let event_details = events[i];
+            console.log("event_details",event_details)
+            if (event_details.event == 'labeled' && event_details.label && event_details.label.name == "p0") {
+                let currentDate = new Date();
+                let labeledDate = new Date(event_details.created_at)
+                console.log("time diff",currentDate - labeledDate)
+                if (currentDate - labeledDate > 0) {
+                    await github.rest.issues.removeLabel({
+                        issue_number: number,
+                        owner: context.repo.owner,
+                        repo: context.repo.repo,
+                        name: "p0"
+
+                    })
+
+                    await github.rest.issues.addLabels({
+                        issue_number: number,
+                        owner: context.repo.owner,
+                        repo: context.repo.repo,
+                        labels:["p1"]
+
+                    })
+                }
+
+            }
+        }
+    }
+}
